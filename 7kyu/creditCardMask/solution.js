c2b79f21ffe0a9de459f923ae376e81f72926e39
haikelz <haikel.2017th@gmail.com>
2023-03-26 11:17:30 +0700

    feat: add new solutions

---
+++ 7kyu/creditCardMask/solution.js
@@ -0,0 +1,19 @@
+/**
+ * Task: buatlah sebuah fungsi bernama maskify yang berfungsi untuk menyembunyikan hampir semua karakter yang diberikan, kecuali 4 karakter terakhir.
+ * Example: input = "242492204234" => output = "24249220****"
+ */
+function maskify(cc) {
+  function confirmSlice(str, start, target) {
+    return str.slice(start, target);
+  }
+
+  const hiddenNum = confirmSlice(cc, 0, cc.length - 4)
+    .split("")
+    .map((value) => (value = "#"))
+    .join("");
+  const showedNum = confirmSlice(cc, cc.length - 4, cc.length);
+
+  return cc.length > 4 ? hiddenNum + showedNum : cc;
+}
+
+console.log(maskify("Skippy")); // expected: ##ippy
