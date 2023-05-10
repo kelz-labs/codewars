84f65302e0b62ef3f42c490f89a3224f85bf100d
haikelz <haikel.2017th@gmail.com>
2023-05-10 21:19:11 +0700

    feat: add new solutions

---
+++ 8kyu/findTheIntegral/solution.js
@@ -0,0 +1,6 @@
+/**
+ * Task: return sesuai format
+ */
+function solution(coefficient, exponent) {
+  return `${coefficient / (exponent + 1)}x^${exponent + 1}`;
+}
---
+++ 8kyu/takeTheDerivative/solution.js
@@ -0,0 +1,6 @@
+/**
+ * Task: return sesuai format
+ */
+function solution(coefficient, exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`;
+}
