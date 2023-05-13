31117a01db5e2c0b04ce4bcb94b0cf6c382b6c88
haikelz <haikel.2017th@gmail.com>
2023-05-13 10:34:35 +0700

    feat: add new solutions

---
+++ 8kyu/switchItUp/solution.js
@@ -0,0 +1,36 @@
+/**
+ * Task: convert number 0-9 ke string penyebutannya. Misal 5 => "Five"
+ */
+function solution(num) {
+  switch (num) {
+    case 0:
+      return "Zero";
+
+    case 1:
+      return "One";
+
+    case 2:
+      return "Two";
+
+    case 3:
+      return "Three";
+
+    case 4:
+      return "Four";
+
+    case 5:
+      return "Five";
+
+    case 6:
+      return "Six";
+
+    case 7:
+      return "Seven";
+
+    case 8:
+      return "Eight";
+
+    case 9:
+      return "Nine";
+  }
+}
---
+++ 8kyu/thinkfulLogicDrills/solution.js
@@ -0,0 +1,12 @@
+function solution(current) {
+  switch (current) {
+    case "green":
+      return "yellow";
+
+    case "yellow":
+      return "red";
+
+    case "red":
+      return "aturan";
+  }
+}
