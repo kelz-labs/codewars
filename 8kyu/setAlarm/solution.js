62c415ea070cec7274df6382af9f3a26b2d5993c
haikelz <haikel.2017th@gmail.com>
2023-05-27 06:15:53 +0700

    feat: add new solution

---
+++ 8kyu/setAlarm/solution.js
@@ -0,0 +1,6 @@
+/**
+ * Task: return true jika employed bernilai true dan vacationnya bernilai false, selain itu return false.
+ */
+function solution(employed, vacation) {
+  return employed && !vacation ? true : false;
+}
