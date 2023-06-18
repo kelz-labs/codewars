8bcc59d32463ac37ffdf871de2110b609b1587fc
haikelz <haikel.2017th@gmail.com>
2023-06-18 13:13:54 +0700

    feat(8kyu): add new solution

---
+++ 8kyu/makeAFunctionThatDoesAritmetic/solution.js
@@ -0,0 +1,22 @@
+/**
+ * Task: lakukan operasi sesuai parameter operator yang diberikan
+ */
+
+function arithmetic(a, b, operator) {
+  switch (operator) {
+    case "add":
+      return a + b;
+
+    case "subtract":
+      return a - b;
+
+    case "multiply":
+      return a * b;
+
+    case "divide":
+      return a / b;
+
+    default:
+      return;
+  }
+}
