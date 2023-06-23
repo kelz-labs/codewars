f5ea0d4af25935f8f781416b2abc5b154cc598f4
haikelz <haikel.2017th@gmail.com>
2023-06-23 19:03:07 +0700

    feat(7kyu): add new solution

---
+++ 7kyu/onesAndZeros/solution.js
@@ -0,0 +1,9 @@
+/**
+ * Task: convert binary ke desimal
+ *
+ * - Dalam hal ini, kita bisa memanfaatkan parseInt().
+ * - Seandainya jika desimal ke binary, maka kita bisa memanfaatkan toString()
+ */
+function solution(arr) {
+  return parseInt(arr.join(), 2);
+}
