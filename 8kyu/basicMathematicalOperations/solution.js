ea2164426b64c30dcb61c69d7bdc46b2d740f026
haikelz <haikel.2017th@gmail.com>
2023-07-28 19:12:44 +0700

    feat(8kyu): add new solution

---
+++ 8kyu/basicMathematicalOperations/solution.js
@@ -0,0 +1,3 @@
+function solution(operation, value1, value2) {
+  return `${value1} ${operation} ${value2}`;
+}
---
+++ 8kyu/totalPressureCalculation/solution.js
@@ -0,0 +1,3 @@
+function solution(M1, M2, m1, m2, v, t) {
+  return ((m1 / M1 + m2 / M2) * (0.082 * (t + 273.15))) / v;
+}
